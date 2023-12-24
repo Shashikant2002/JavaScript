53d01876c1851c75e76e86e2c7cc06cf438b6d4b
adiogentgit <adiogentgit@gmail.com>
2023-12-24 15:18:56 +0530

    ...

--- TypeScript/ts_1/project/index.js
+++ TypeScript/ts_1/project/index.js
@@ -1,8 +1,13 @@
 "use strict";
-const sum = (a, b) => {
-    console.log(a + b);
-    return a + b;
-};
-console.log(sum(1, 2));
-const bigInt = Number.MAX_SAFE_INTEGER;
-console.log(bigInt + 1);
+// const sum = (a: number, b: number): number => {
+//   console.log(a + b);
+//   return a + b;
+// };
+// console.log(sum(1, 2));
+// const bigInt: number = Number.MAX_SAFE_INTEGER;
+// console.log(bigInt + 1);
+function greet(name) {
+    console.log(`Good Morning ${name}`);
+    return `Good Morning ${name}`;
+}
+console.log(greet("Shashiknat"));
--- TypeScript/ts_1/project/index.ts
+++ TypeScript/ts_1/project/index.ts
@@ -1,10 +1,18 @@
-const sum = (a: number, b: number): number => {
-  console.log(a + b);
-  return a + b;
-};
+// const sum = (a: number, b: number): number => {
+//   console.log(a + b);
+//   return a + b;
+// };
 
-console.log(sum(1, 2));
+// console.log(sum(1, 2));
 
-const bigInt: number = Number.MAX_SAFE_INTEGER;
+// const bigInt: number = Number.MAX_SAFE_INTEGER;
 
-console.log(bigInt + 1);
+// console.log(bigInt + 1);
+
+
+function greet(name: string): string {
+  console.log(`Good Morning ${name}`);
+  return `Good Morning ${name}`;
+}
+
+console.log(greet("Shashiknat"));
